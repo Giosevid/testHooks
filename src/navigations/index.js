69175875ca5d4dc2ba9081013b2526c03a3d3967
GIosevid <giosevid@gmail.com>
2020-09-11 11:41:21 -0300

    Stack navigator

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -5,42 +5,87 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Button from '../components/commons/Button/Button';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Input from '../components/commons/Input';
-import Menu from './Menu';
 import { navigationRef } from './NavigationRoot';
 import * as NavigationRoot from './NavigationRoot'
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 function HomeScreen(props) {
-  const actionButton = () => NavigationRoot.navigate('Perfiles');
+  const actionButton = () => NavigationRoot.navigate('Detail');
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
       <Input placeholder="correo electrónico" />
-      <Button action={actionButton} label='Ir a' />
+      <Button action={actionButton} label='Ir a Detail' />
     </View>
   );
 }
 
-function Perfiles(props) {
-  const actionButton = () => props.navigation.navigate('Home')
+function DetailScreen(props) {
+  const actionButtonPush = () => props.navigation.push('Detail');
+  const actionButton = () => props.navigation.navigate('Home');
+  const actionButtonBack = () => props.navigation.goBack();
+  const actionButtonPop = () => props.navigation.popToTop();
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Perfil Screen</Text>
-      <Button action={actionButton}  label='Ir a' />
+      <Text>Detail Screen</Text>
+      <Button action={actionButtonPush}  label='Ir a Detail de nuevo' />
+      <Button action={actionButton}  label='Ir a Home' />
+      <Button action={actionButtonBack}  label='Ir a atras..' />
+      <Button action={actionButtonPop}  label='Ir a la primera pantalla..' />
     </View>
   );
 }
 
-const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
+const DetailStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+
+const HomeStackScreen = ({navigation}) => (
+  <HomeStack.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor: "#009387",
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      fontWeight: 'bold'
+    }
+  }}>
+    <HomeStack.Screen name="Home" component={HomeScreen} options={{
+      title: 'Inicio',
+      headerLeft: () => (
+        <Icon.Button name="ios-menu" size={25} backgroundColor="#009387" onPress={() => navigation.openDrawer()}/>
+      )
+    }}/>
+  </HomeStack.Navigator>
+)
+
+const DetailStackScreen = ({navigation}) => (
+  <DetailStack.Navigator screenOptions={{
+    headerStyle: {
+      backgroundColor: "#009387",
+    },
+    headerTintColor: "#fff",
+    headerTitleStyle: {
+      fontWeight: 'bold'
+    }
+  }}>
+    <DetailStack.Screen name="Detail" component={DetailScreen} options={{
+      headerLeft: () => (
+        <Icon.Button name="ios-menu" size={25} backgroundColor="#009387" onPress={() => navigation.openDrawer()}/>
+      )
+    }} />
+  </DetailStack.Navigator>
+)
 
 function RootNavigator() {
   return (
     <NavigationContainer ref={navigationRef}>
-      <Drawer.Navigator drawerContent={(props) => <Menu {...props} />}>
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Perfiles" component={Perfiles} />
+      <Drawer.Navigator>
+        <Drawer.Screen name="Home" component={HomeStackScreen}/>
+        <Drawer.Screen name="Detail" component={DetailStackScreen}/>
       </Drawer.Navigator>
     </NavigationContainer>
   );
